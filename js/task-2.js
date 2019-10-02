b18700e88ce45b8227ac979eb49dc4346418a3e4
Oleksandr Kryvenets <kralvi987@gmail.com>
2019-10-02 13:51:35 +0300

    added hw-04

---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+const inventory = {
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter((item) => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = (itemName, action) => {
+  console.log(`Invoking ${action.name} opeartion on ${itemName}`);
+  action.call(inventory, itemName);
+};
+
+invokeInventoryAction('Аптечка', inventory.add);
+// Invoking add opeartion on Аптечка
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryAction('Фильтр', inventory.remove);
+// Invoking remove opeartion on Фильтр
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
